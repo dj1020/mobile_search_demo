0be4a6263fe2a93087f2fb7745b4463b1248f0d0
Ken Lin <ken.lin@cloud-interactive.com>
2016-02-21 12:12:48 +0800

    Apply bootstrap CSS style, and fix camera pixel field value

--- resources/views/search/result.blade.php
+++ resources/views/search/result.blade.php
@@ -3,22 +3,31 @@
 <head>
     <meta charset="UTF-8">
     <title>Mobile Search Page</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css"/>
+    <style>
+        form {
+            padding-bottom: 10px;
+        }
+    </style>
 </head>
 <body>
-    <h1>Search Result</h1>
-    <div>
+    <div class="container">
+        <h1>Search Result</h1>
 
         <form method="get">
+            每頁
             <select name="perPage" id="perPage" onChange="this.form.submit()">
                 @foreach ($perPageSelect as $val)
                     <option value="{{ $val }}"  {{ $mobiles->perPage() == $val ? 'selected' : '' }}>{{ $val }}</option>
                 @endforeach
             </select>
+            筆
+            <span class="pull-right">第 {{ $mobiles->currentPage() }} / {{ $mobiles->lastPage() }} 頁，總筆數 {{ $mobiles->total() }} 筆</span>
         </form>
 
-        <p>第 {{ $mobiles->currentPage() }} / {{ $mobiles->lastPage() }} 頁，總筆數 {{ $mobiles->total() }} 筆</p>
 
-        <table>
+
+        <table class="table">
             <tr>
                 <th>ID</th>
                 <th>Thumbnail</th>
@@ -27,7 +36,7 @@
                 <th>Monitor Size(")</th>
                 <th>Weight(g)</th>
                 <th>Rom Size(GB)</th>
-                <th>Camera Pixel(MP)</th>
+                <th>Camera Pixel</th>
                 <th>Memory Slot</th>
             </tr>
             @foreach ($mobiles as $mobile)
@@ -39,7 +48,7 @@
                 <td>{{ $mobile->monitor_size }}</td>
                 <td>{{ $mobile->weight }}</td>
                 <td>{{ $mobile->rom }}</td>
-                <td>{{ $mobile->camera_size }}</td>
+                <td>{{ $mobile->camera_pixel }} 萬像素</td>
                 <td>{{ ($mobile->has_memory_slot) ? "Yes" : "No" }}</td>
             </tr>
             @endforeach
