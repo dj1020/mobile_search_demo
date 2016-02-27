6619a13f97789c2c75e7a48afafbf3a7795be970
Ken Lin <ken.lin@cloud-interactive.com>
2016-02-27 16:51:10 +0800

    Added cancel button to back to mobile list

--- resources/views/mobiles/create.blade.php
+++ resources/views/mobiles/create.blade.php
@@ -129,6 +129,7 @@
                 <div class="col-sm-9">
                     <div class="col-sm-offset-3 col-sm-9">
                         <button type="submit" class="btn btn-primary">Add</button>
+                        <a href="{{ url('mobiles') }}" class="btn btn-default">Cancel</a>
                     </div>
                 </div>
             </div>
